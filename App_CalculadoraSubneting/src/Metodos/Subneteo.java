59911b8dae9512c5f807ec7b10684283028e0246
Brujo <Brujo@172.16.250.100>
2015-11-20 09:58:30 -0300

    Se realizan los métodos:
    Paso 2 : Obtener la Mascara a partir de la clase -> "mascaraClase()"
    Paso 3 : Obtener la Mascara de SubRed a partir de "mascaraClase()" -> "mascaraSubRed()"

--- App_CalculadoraSubneting/src/Metodos/Subneteo.java
+++ App_CalculadoraSubneting/src/Metodos/Subneteo.java
@@ -13,9 +13,11 @@ package Metodos;
 public class Subneteo {
     
     private String direccionIP;
+    private int subRedes;
     
-    public Subneteo(String direccionIP) {
+    public Subneteo(String direccionIP,int subRedes) {
         this.direccionIP = direccionIP;
+        this.subRedes = subRedes;
     }
     
     
@@ -33,7 +35,7 @@ public class Subneteo {
         int primerOcteto= Integer.parseInt(arregloDireccion[0]);
         String clase="";
         
-        if (primerOcteto < 127) {
+        if (primerOcteto <= 127) {
             clase="Clase A";
         }else if(primerOcteto >= 128 && primerOcteto <= 191){
             clase="Clase B";
@@ -42,10 +44,67 @@ public class Subneteo {
         }else if(primerOcteto >= 224 && primerOcteto <= 239){
             clase="Clase D";
         }else if(primerOcteto >= 240 && primerOcteto <= 255){
-            clase="Clase D";
+            clase="Clase E";
         }
         
         return clase;
+    }
+    
+    // Paso 2: Obtener la masacara a partir de la clase by JBY
+    // Retorna String de Mascara ejemplo Clase A: 8 
+    //
+    // Clase A :  8
+    // Clase B : 16
+    // Clase C : 24
+    // Clase D :  0 Se manejará en la GUI
+    // Clase E :  0 Se manejará en la GUI
+    //
+    public String mascaraClase(){
+        String mascara="0";
+        String clase=obtenerClase();
+        
+        if (clase.equalsIgnoreCase("Clase a")) {
+            mascara="8";
+        }else if (clase.equalsIgnoreCase("Clase b")) {
+            mascara="16";
+        }else if (clase.equalsIgnoreCase("Clase c")) {
+            mascara="24";
+        }else if (clase.equalsIgnoreCase("Clase d")) {
+            mascara="0";
+        }else if (clase.equalsIgnoreCase("Clase e")) {
+            mascara="0";
+        }
+        
+        return mascara;
+    }
+    
+    // Paso 3: Obtener la mascaraSubred a partir de la MascaraClase by JBY
+    // Retorna int : MascaraSubred + MascaraClase
+    //
+    // Clase A :  8
+    // Clase B : 16
+    // Clase C : 24
+    // Clase D :  0 Se manejará en la GUI
+    // Clase E :  0 Se manejará en la GUI
+    //
+    public int mascaraSubRed(){
+        int subRedes = this.subRedes;
+        int mascaraClase = Integer.parseInt(mascaraClase());
+        int mascaraSubRed=0;
+        
+        int bits=2;
+        
+        for (int i = 0; i < 8; i++) {
+            int exponente = (int) Math.pow(bits, i);
+            int buscandoRango = exponente - subRedes;
+            
+            if (buscandoRango>=0) {
+                mascaraSubRed=i;
+                i=8; //Cortando el loop
+            }
+        }
+        
+        return mascaraSubRed+mascaraClase;
     }
 
 }
